258e1f9c3908bd3d818fe20d420c245747226e3a
myeonghee <61863525+myg9911@users.noreply.github.com>
2022-11-02 18:45:26 +0900

    cc

--- index.js
+++ index.js
@@ -1,41 +1,7 @@
 import '@babel/polyfill';
 import 'url-polyfill';
-import './src/vue/excel/import';
-import cm from './src/component/ViewComponent';
-import Deferred from './src/util/Deferred';
-import om from './src/service/OperationManager';
-import sm, { callGeneralService, callGridCandidateService, callGridColumnService, callReferService, callService, doReferenceService, getCompleteParamMap } from './src/service/ServiceManager';
-import xl from './src/util/ExcelTool';
-import ViewComponent from './src/component/ViewComponent'
-import { gFileStorageCategory, gHttpStatus, gOperationType } from './src/vue/const';
 
-export {
-  callGeneralService,
-  callGridCandidateService,
-  callGridColumnService,
-  callReferService,
-  callService,
-  cm,
-  Deferred,
-  doReferenceService,
-  getCompleteParamMap,
-  gFileStorageCategory,
-  gHttpStatus,
-  gOperationType,
-  om,
-  sm,
-  xl
-};
-
-export * from './src/component/grid/gridFunc';
-export * from './src/util/dialog';
-export * from './src/util/polyfill';
-export * from './src/util/utils';
-export * from './src/util/waitMe';
-export * from './src/vue/kendo';
-export * from './src/vue/i18n';
-export * from './src/vue/utils';
-export * from './src/vue/grid';
-export * from './src/vue/excel/import';
-export * from './src/vue/excel/export';
-export * from './src/vue/pageNavigator';
+export * from './src/component';
+export * from './src/service';
+export * from './src/util';
+export * from './src/vue';
